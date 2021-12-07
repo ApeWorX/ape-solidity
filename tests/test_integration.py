193071a97e58b76953820fdbfd18c174e286b489
unparalleled-js <yingthi@live.com>
2021-12-07 13:19:05 -0600

    chore: change var name in tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
-TEST_PROJECTS = [str(p.stem) for p in (Path(__file__).parent / "contracts").iterdir()]
+TEST_CONTRACTS = [str(p.stem) for p in (Path(__file__).parent / "contracts").iterdir()]
 
 
 def test_integration(project):
-    for proj in TEST_PROJECTS:
-        assert proj in project.contracts
-        contract = project.contracts[proj]
-        assert f"contracts/{proj}.sol" in contract.sourceId
+    for contract in TEST_CONTRACTS:
+        assert contract in project.contracts
+        contract = project.contracts[contract]
+        assert f"contracts/{contract}.sol" in contract.sourceId
