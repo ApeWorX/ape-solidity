5246c7505064875444e3eaa20ffd0df35468a83c
unparalleled-js <yingthi@live.com>
2022-08-30 09:58:34 -0500

    refactor: raise when given invalid files

--- ape_solidity/_utils.py
+++ ape_solidity/_utils.py
@@ -3,6 +3,7 @@ import re
 from pathlib import Path
 from typing import Dict, List, Optional, Set, Union
 
+from ape.exceptions import CompilerError
 from ape.logging import logger
 from semantic_version import NpmSpec  # type: ignore
 
@@ -78,4 +79,9 @@ def load_dict(data: Union[str, dict]) -> Dict:
 
 
 def verify_contract_filepaths(contract_filepaths: List[Path]) -> Set[Path]:
-    return {p for p in contract_filepaths if p.suffix == ".sol"}
+    invalid_files = [p.name for p in contract_filepaths if p.suffix != ".sol"]
+    if not invalid_files:
+        return set(contract_filepaths)
+
+    sources_str = "', '".join(invalid_files)
+    raise CompilerError(f"Unable to compile '{sources_str}' using Solidity compiler.")
